fc421109389ee15e7a5e09a67fad7f307eff9be1
Rohinton Kazak <rohinton.kazak@gmail.com>
2015-11-30 08:36:43 -0800

    added function to get individual todo items

--- server.js
+++ server.js
@@ -1,10 +1,68 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'Meet someone for lunch',
+	compleated: false,
+},{
+	id: 2,
+	description: 'Goto market',
+	compleated: false,
+},{
+	id: 3,
+	description: 'Get learning!',
+	compleated: false,
+}];
 
 app.get('/', function( req, res ) {
 	res.send('Todo API Root');
 });
+
+// GET /todos
+app.get('/todos',function(req, res) {
+	res.json(todos);
+});
+
+// GET /todos/:id
+app.get('/todos/:id', function ( req, res ) {
+	var todoId = req.params.id;
+	//console.log('todoid '+todoId);
+	// Iterate over todos array. Find match.
+	//console.log('len '+todos.length);
+	for (var i in todos) {
+		var val = todos[i];
+		//console.log(val['id']+' '+todoId);
+
+		if (val['id'] == todoId) {
+			//console.log('Yi');
+			//res.send('Asking for todo with id of ' + req.params.id);
+			res.json(val);
+		}
+	};
+	res.status(404).send();
+});
+
+/*
+Alternate -
+app.get('/todos:id', function( req, res ) {
+	var todoID = parseInt(req.params.id, 10);
+	var matchedTodo;
+
+	todos.forEach(function (todo) {
+	   if (todoId === todo.id) {
+	      matchedTodo = todo;
+	   }
+	});
+
+	if (matchedTodo) {
+	   res.json(matchedTodo);
+	} else {
+	   res.status(404).send();
+	}
+});
+
+*/
 
 app.listen(PORT, function() {
 	console.log('Express listening on port ' + PORT + '!');
