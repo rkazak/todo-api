7175995bfafb7a39f3c63050e56cd03e7f7b97e1
Rohinton Kazak <rohinton.kazak@gmail.com>
2015-12-10 18:46:05 -0800

    Hash and salt password

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.post('/todos', function(req, res) {
 app.post('/users', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 	db.user.create(body).then( function (user) {
-		res.json(user.toJSON());
+		res.json(user.toPublicJSON());
 	}, function (e) {
 		res.status(400).json(e);
 	});
@@ -129,7 +129,7 @@ app.put('/todos/:id', function(req, res) {
 	});
 });
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
 	});
