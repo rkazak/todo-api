2f7542a733f97f099494b7c2af11a875583f9ad8
Rohinton Kazak <rohinton.kazak@gmail.com>
2015-12-10 18:03:15 -0800

    Convert email to lowercase.

--- models/user.js
+++ models/user.js
@@ -15,6 +15,14 @@ module.exports = function(sequelize, DataTypes) {
 				len: [6,50]
 			}
 		}
+	}, {
+		hooks: {
+			beforeValidate: function ( user, options) {
+				if (typeof user.email === 'string') {
+					user.email = user.email.toLowerCase();
+				}
+			}
+		}
 	});
 
 };
